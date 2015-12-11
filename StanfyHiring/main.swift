2218097465aad3219320ef722d3d370d1cee7e81
Vitaliy Malakhovskiy <purpleshirted@gmail.com>
2015-12-11 17:30:16 +0200

    Initial Commit

---
+++ StanfyHiring/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StanfyHiring
+//
+//  Created by Vitaliy Malakhovskiy on 12/11/15.
+//  Copyright © 2015 Vitalii Malakhovskyi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
